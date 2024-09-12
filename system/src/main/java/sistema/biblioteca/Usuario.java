2d84edb208102f6456e177d60a2e568e57b9dd40
gust_rib <gu_crv1@hotmail.com>
2024-09-12 12:58:31 -0300

    Criando os atributos e métodos da classe usuario que herda Pessoa

--- system/src/main/java/sistema/biblioteca/Usuario.java
+++ system/src/main/java/sistema/biblioteca/Usuario.java
@@ -1,5 +1,42 @@
 package sistema.biblioteca;
 
-public class Usuario {
+public class Usuario extends Pessoa {
+    private String cpf;
+    private String email;
+    private String senha;
 
+    public Usuario(String nome, String cpf, String email, String senha) {
+        super(nome);
+        this.cpf = cpf;
+        this.email = email;
+        this.senha = senha;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getIdentificacao() {
+        return cpf;
+    }
 }
