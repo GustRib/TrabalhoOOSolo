70738d0ec65b1ae271c38cfaa694ecc0abec6767
gust_rib <gu_crv1@hotmail.com>
2024-09-12 12:34:20 -0300

    criando os atributos e métodos da classe Pessoa

--- system/src/main/java/sistema/biblioteca/Pessoa.java
+++ system/src/main/java/sistema/biblioteca/Pessoa.java
@@ -1,5 +1,19 @@
 package sistema.biblioteca;
 
 public abstract class Pessoa {
+    protected String nome;
 
+    public Pessoa(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public abstract String getIdentificacao();
 }
