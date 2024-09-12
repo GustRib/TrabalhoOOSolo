dc90589b9f9073832ce7d55ca4f691bc94c45782
gust_rib <gu_crv1@hotmail.com>
2024-09-12 14:24:00 -0300

    Criando minha classe de UsuarioEstudante(Um dos perfis de acesso do sistema) que herda a classe Usuario

--- system/src/main/java/sistema/biblioteca/UsuarioEstudante.java
+++ system/src/main/java/sistema/biblioteca/UsuarioEstudante.java
@@ -1,5 +1,23 @@
 package sistema.biblioteca;
 
-public class UsuarioEstudante {
+public class UsuarioEstudante extends Usuario{
+    private int limiteEmprestimos = 3;
+    private String nivelAcesso;
 
+    public UsuarioEstudante(String nome, String cpf, String email, String senha, String nivelAcesso) {
+        super(nome, cpf, email, senha);
+        this.nivelAcesso = nivelAcesso;
+    }
+
+    public int getLimiteEmprestimos() {
+        return limiteEmprestimos;
+    }
+
+    public String getNivelAcesso() {
+        return nivelAcesso;
+    }
+
+    public void setNivelAcesso(String nivelAcesso) {
+        this.nivelAcesso = nivelAcesso;
+    }
 }
