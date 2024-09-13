419e18e06d9a32998e476785f63ae189ead30796
gust_rib <gu_crv1@hotmail.com>
2024-09-13 10:48:15 -0300

    mudando o nome da minha classe UsuarioEstudante para Estudante

--- system/src/main/java/sistema/biblioteca/Estudante.java
+++ system/src/main/java/sistema/biblioteca/Estudante.java
@@ -1,10 +1,10 @@
 package sistema.biblioteca;
 
-public class UsuarioEstudante extends Usuario{
+public class Estudante extends Usuario{
     private int limiteEmprestimos = 3;
     private String nivelAcesso;
 
-    public UsuarioEstudante(String nome, String cpf, String email, String senha, String nivelAcesso) {
+    public Estudante(String nome, String cpf, String email, String senha, String nivelAcesso) {
         super(nome, cpf, email, senha);
         this.nivelAcesso = nivelAcesso;
     }
