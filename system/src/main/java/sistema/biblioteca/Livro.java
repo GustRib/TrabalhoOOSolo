2597b4a889cd0e66ea978db5b8baf6ceea3d981b
gust_rib <gu_crv1@hotmail.com>
2024-09-15 21:52:21 -0300

    Criando a classe livro. Fiquei quebrando cabeça com o erro Syntax error on token String, record expected, mas consegui resolver refatorando o código

--- system/src/main/java/sistema/biblioteca/Livro.java
+++ system/src/main/java/sistema/biblioteca/Livro.java
@@ -1,5 +1,40 @@
 package sistema.biblioteca;
 
 public class Livro {
+    private String titulo;
+    private String autor;
+    private String isbn;
+    private boolean disponivel;
+    private static int totalLivros;
 
+    public Livro(String titulo, String autor, String isbn) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.isbn = isbn;
+        this.disponivel = true;
+        totalLivros++;
 }
+        public String getTitulo(){
+            return titulo;
+        }
+
+        public String getAutor() {
+            return autor;
+        }
+
+        public String getIsbn() {
+            return isbn;
+        }
+        
+        public boolean isDisponivel() {
+            return disponivel;
+        }
+
+        public void setDisponivel(boolean disponivel) {
+            this.disponivel = disponivel;
+        }
+
+        public static int getTotalLivros() {
+            return totalLivros;
+        }
+    }
