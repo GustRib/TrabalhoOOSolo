50426a33416c891e3e7529cbdafe954384d3767e
gust_rib <gu_crv1@hotmail.com>
2024-09-15 21:58:51 -0300

    Criando a classe Emprestimo. Optei por utilizar a biblioteca LocalDate de Java para settar as datas de Emprestimo e Devolucao, vamos ver se vai funcionar

--- system/src/main/java/sistema/biblioteca/Emprestimo.java
+++ system/src/main/java/sistema/biblioteca/Emprestimo.java
@@ -1,5 +1,36 @@
 package sistema.biblioteca;
+import java.time.LocalDate;
 
 public class Emprestimo {
+    private Livro livro;
+    private Usuario usuario;
+    private LocalDate dataEmprestimo;
+    private LocalDate dataDevolucao;
 
+    public Emprestimo(Livro livro, Usuario usuario) {
+        this.livro = livro;
+        this.usuario = usuario;
+        this.dataEmprestimo = LocalDate.now();
+    }
+
+    public Livro getLivro() {
+        return livro;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public LocalDate getDataEmprestimo() {
+        return dataEmprestimo;
+    }
+
+    public LocalDate getDataDevolucao() {
+        return dataDevolucao;
+    }
+
+    public void devolver() {
+        this.dataDevolucao = LocalDate.now();
+        livro.setDisponivel(true);
+    }
 }
