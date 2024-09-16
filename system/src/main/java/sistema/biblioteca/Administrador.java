b6eac443970d629b7d2fe3e5245fcce980abdb11
gust_rib <gu_crv1@hotmail.com>
2024-09-15 21:31:40 -0300

    Criando a classe Administrador que herda Usuario

--- system/src/main/java/sistema/biblioteca/Administrador.java
+++ system/src/main/java/sistema/biblioteca/Administrador.java
@@ -1,5 +1,38 @@
 package sistema.biblioteca;
 
-public class Administrador {
+public class Administrador extends Usuario{
+    private String nivelAcesso;
+    private boolean gerenciarUsuario;
+    private boolean gerenciarSistema;
 
+    public Administrador(String nome, String cpf, String email, String senha, String nivelAcesso, boolean gerenciarUsuario, boolean gerenciarSistema) {
+        super(nome, cpf, email, senha);
+        this.nivelAcesso = nivelAcesso;
+        this.gerenciarUsuario = gerenciarUsuario;
+        this.gerenciarSistema = gerenciarSistema;
+    }
+
+    public String getNivelAcesso() {
+        return nivelAcesso;
+    }
+
+    public void setNivelAcesso(String nivelAcesso) {
+        this.nivelAcesso = nivelAcesso;
+    }
+
+    public boolean isGerenciarUsuario() {
+        return gerenciarUsuario;
+    }
+
+    public void setGerenciarUsuario(boolean gerenciarUsuario) {
+        this.gerenciarUsuario = gerenciarUsuario;
+    }
+
+    public boolean isGerenciarSistema() {
+        return gerenciarSistema;
+    }
+
+    public void setGerenciarSistema(boolean gerenciarSistema) {
+        this.gerenciarSistema = gerenciarSistema;
+    }
 }
